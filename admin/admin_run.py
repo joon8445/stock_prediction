6e256887ffbb1c4a0051d61e12b8187c85a171f5
ksjun <joon8445@gmail.com>
2021-11-26 13:54:32 +0900

    예측값 존재하면 pass

--- admin/admin_run.py
+++ admin/admin_run.py
@@ -1,4 +1,6 @@
 from admin_data_updater import data_updater
+import os
+import pandas as pd
 
 if __name__ == '__main__':
     stock_list = {'005930': '삼성전자', '000660': 'SK하이닉스', '035420': 'NAVER', '035720': '카카오'}
@@ -6,6 +8,14 @@ if __name__ == '__main__':
     for code, company in stock_list.items():
         print(code, company)
         updater.update_stock_price(code, company)
-        updater.update_prediction(code, company)
+        pred_file = f'./data/{code}_{company}_pred.csv'
+        stock_file = f'./data/{code} {company}.csv'
+        if os.path.exists(pred_file):
+            predict = pd.read_csv(pred_file)
+            stock = pd.read_csv(stock_file)
+            if predict.date.iloc[-1] == stock.date.iloc[-1]:
+                print('already predicted')
+        else:
+            updater.update_prediction(code, company)
 
 """run"""
