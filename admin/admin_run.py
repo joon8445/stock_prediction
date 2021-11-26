893d12285c2138a2e2dec974e1d258314538db07
ksjun <joon8445@gmail.com>
2021-11-26 11:25:11 +0900

    admin run

--- admin/admin_run.py
+++ admin/admin_run.py
@@ -1,1 +1,11 @@
+from admin_data_updater import data_updater
+
+if __name__ == '__main__':
+    stock_list = {'005930': '삼성전자', '000660': 'SK하이닉스', '035420': 'NAVER', '035720': '카카오'}
+    updater = data_updater()
+    for code, company in stock_list.items():
+        print(code, company)
+        updater.update_stock_price(code, company)
+        updater.update_prediction(code, company)
+
 """run"""
