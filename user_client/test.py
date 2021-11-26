a93e3a5a8ca92c4b6f5e74d5643391273056d169
ksjun <joon8445@gmail.com>
2021-11-26 11:25:28 +0900

    파일 연결 test

--- user_client/test.py
+++ user_client/test.py
@@ -2,6 +2,7 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
@@ -12,15 +13,22 @@ form_class = uic.loadUiType("test.ui")[0]
 
 class WindowClass(QMainWindow, form_class):
     def __init__(self):
+        stock = pd.read_csv('./../admin/data/005930 삼성전자.csv')
+        predict = pd.read_csv('./../admin/data/005930_삼성전자_pred.csv')
         super().__init__()
         self.setupUi(self)
         self.fig = plt.Figure()
 
-        real_price = 110000
+        real_price = stock.close.iloc[-1]
+        real_price_date = stock.date.iloc[-1]
 
-        predict_price_1 = 113000
-        prediction_1 = '2021. 11. 03. Data 기준 11.4일 예측가 : '+str(predict_price_1)+'\n전일 거래 예측 종가 오차율 : '
-        prediction_1 += "{:.2f}".format((1 - real_price / predict_price_1)*100)
+        predict_price_RNN = predict.percent.iloc[-1]
+        predict_price_RNN_1 = predict.percent.iloc[-1]
+        predict_price_RNN_1_date = predict.date.iloc[-1]
+
+        predict_price_1 = 72000
+        prediction_1 = real_price_date + '종가 기준 다음 거래일 종가 예측 : '+predict_price_RNN+'\n전일 거래 예측 종가 오차율 : '
+        prediction_1 += predict_price_RNN_1
         predict_price_2 = 114000
         prediction_2 = '2021. 11. 03. Data 기준 11.4일 예측가 : ' + str(predict_price_1) + '\n전일 거래 예측 종가 오차율 : '
         prediction_2 += "{:.2f}".format((1 - real_price / predict_price_2) * 100)
@@ -30,18 +38,11 @@ class WindowClass(QMainWindow, form_class):
         self.canvas = FigureCanvas(self.fig)
 
         self.chart.addWidget(self.canvas)
-        yf.pdr_override()
-        print('override')
-        sec = pdr.get_data_yahoo('005930.KS', start='2018-05-04')  # kospi = XXXXXX.KS
-        msft = pdr.get_data_yahoo('MSFT', start='2018-05-04')  # kosdaq = symbol
-        print('get_data')
-        tmp_msft = msft.drop(columns='Volume')
-        tmp_msft.tail()
 
 
         # plot(x, y, 마커 형태,[,label='Label'])
         ax = self.fig.add_subplot(111)
-        ax.plot(sec.index, sec.Close, 'b', label='SSEC')  # x = dateindex, y = Close,
+        ax.plot(stock.index, stock.close, 'b', label='SSEC')  # x = dateindex, y = Close,
         ax.legend(loc='best')
 
 if __name__== "__main__" :
