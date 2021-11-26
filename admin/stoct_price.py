3407f3faeafd5fe75f42d5a8c445144b5bbaabe2
ksjun <joon8445@gmail.com>
2021-11-26 11:24:58 +0900

    주식값 불러오기

--- admin/stoct_price.py
+++ admin/stoct_price.py
@@ -1,5 +1,25 @@
-import pandas as pd;
-def load_stock_price():
+import pandas as pd
+from datetime import datetime
+
+
+def load_stock_pricrea():
     """야후 파이낸스에서 주식값 불러오기"""
-    # return df '주식정보'
+
+def read_yahoo(code, company):
+    """Yahoo Finance에서 주식 시세를 읽어서 데이터프레임으로 반환"""
+    try:
+        url = f'https://query1.finance.yahoo.com/v7/finance/download/%s?period1=0&period2=1000000000000000000&interval=1d&events=history&includeAdjustedClose=true' % (
+                    code + '.KS')
+        df = pd.read_csv(url)
+        tmnow = datetime.now().strftime('%Y-%m-%d %H:%M')
+        print('[{}] {} ({}) : downloading...'.format(tmnow, company, code), end="\r")
+        df = df.rename(columns={'Date': 'date', 'Open': 'open', 'High': 'high', 'Low': 'low',
+                                'Close': 'close', 'Adj Close': 'adjc', 'Volume': 'volume'})
+        df = df.dropna()
+        df[['open', 'high', 'low', 'close', 'adjc', 'volume']] = df[
+            ['open', 'high', 'low', 'close', 'adjc', 'volume']].astype(int)
+    except Exception as e:
+        print('Exception occured', str(e))
+        return None
+    return df
 
