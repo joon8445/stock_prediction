58dbf39403992e220fff38dc9b0358b3f15c1563
ksjun <joon8445@gmail.com>
2021-11-26 11:24:00 +0900

    예측 값 저장

--- admin/admin_data_updater.py
+++ admin/admin_data_updater.py
@@ -1,28 +1,20 @@
 import pandas as pd
-from datetime import datetime
+from stoct_price import read_yahoo
+import prediction
+import os
 
 class data_updater:
     def update_stock_price(self, code, company):
         """주식 값 저장"""
-        self.read_yahoo(code, company).to_csv(f'./data/{code} {company}.csv', index=False)
+        read_yahoo(code, company).to_csv(f'./data/{code} {company}.csv', index=False)
 
-    def read_yahoo(self, code, company):
-        """Yahoo Finance에서 주식 시세를 읽어서 데이터프레임으로 반환"""
-        try:
-            url = f'https://query1.finance.yahoo.com/v7/finance/download/%s?period1=0&period2=1000000000000000000&interval=1d&events=history&includeAdjustedClose=true' % (
-                        code + '.KS')
-            df = pd.read_csv(url)
-            tmnow = datetime.now().strftime('%Y-%m-%d %H:%M')
-            print('[{}] {} ({}) : downloading...'.format(tmnow, company, code), end="\r")
-            df = df.rename(columns={'Date': 'date', 'Open': 'open', 'High': 'high', 'Low': 'low',
-                                    'Close': 'close', 'Adj Close': 'adjc', 'Volume': 'volume'})
-            df = df.dropna()
-            df[['open', 'high', 'low', 'close', 'adjc', 'volume']] = df[
-                ['open', 'high', 'low', 'close', 'adjc', 'volume']].astype(int)
-        except Exception as e:
-            print('Exception occured', str(e))
-            return None
-        return df
-    def update_prediction(self):
+    def update_prediction(self,code, company):
         """예측 값 저장"""
-
+        date, code, company, percentage = prediction.RNN(code, company)
+        pred = {'date': [date], 'percent': [percentage]}
+        pred_data = pd.DataFrame(pred)
+        file = f'./data/{code}_{company}_pred.csv'
+        if not os.path.exists(file):
+            pred_data.to_csv(file, index=False, mode='w', encoding='utf-8-sig')
+        else:
+            pred_data.to_csv(file, index=False, mode='a', encoding='utf-8-sig', header=False)
